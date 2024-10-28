5f6889d5b4e6b88bc000fe940ae4d1e4f86a9137
ZeenJung <6531501145@lamduan.mfu.ac.th>
2024-10-28 17:23:45 +0700

    Update Check_status.dart

---
+++ lib/User(student)/Check_status.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Status_check extends StatelessWidget {
+  const Status_check({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
