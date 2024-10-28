160d6c1726809dd07c87c929f07466ed7c9f7a8b
ZeenJung <6531501145@lamduan.mfu.ac.th>
2024-10-28 17:10:58 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:project_mobileapp/Staff/home_staff.dart';
+import 'package:project_mobileapp/User(student)/Profile.dart';
 import 'package:project_mobileapp/User(student)/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: ProfileApp(),
     // theme: ThemeData(),
     debugShowCheckedModeBanner: false,
   ));
