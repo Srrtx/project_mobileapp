377f3050edf4c2ff1c4be2409d03249c84c09b5a
Srrt <seatsiri.d@gmail.com>
2024-10-29 02:04:18 +0700

    update home user (select room)

--- lib/User(student)/home.dart
+++ lib/User(student)/home.dart
@@ -13,14 +13,11 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('All Room'),
-        backgroundColor: Colors.transparent,
-        elevation: 0, // Remove shadow
         titleTextStyle: const TextStyle(
           color: Colors.black,
           fontSize: 28,
           fontWeight: FontWeight.bold,
         ),
-        // centerTitle: true,
       ),
       body: SafeArea(
         child: Padding(
@@ -36,7 +33,7 @@ class Home extends StatelessWidget {
                     padding:
                         const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                     decoration: BoxDecoration(
-                      color: Color.fromRGBO(240, 235, 227, 1),
+                      color: const Color.fromRGBO(240, 235, 227, 1),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Row(
@@ -55,7 +52,7 @@ class Home extends StatelessWidget {
                     padding:
                         const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                     decoration: BoxDecoration(
-                      color: Color.fromRGBO(240, 235, 227, 1),
+                      color: const Color.fromRGBO(240, 235, 227, 1),
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Row(
@@ -99,27 +96,7 @@ class Home extends StatelessWidget {
           ),
         ),
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const [
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.notifications),
-      //       label: 'Notifications',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.access_time),
-      //       label: 'Schedule',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.account_balance),
-      //       label: 'Profile',
-      //     ),
-      //   ],
-      // ),
-
+      //Nav bar (bottom)
       bottomNavigationBar: NavigationBar(
         height: 60,
         elevation: 0,
@@ -133,71 +110,8 @@ class Home extends StatelessWidget {
               icon: Icon(Icons.account_circle), label: 'Profile'),
         ],
       ),
-
-      //FAB for add room
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     // Action to add a room (e.g., show a dialog or navigate to another screen)
-      //     _showAddRoomDialog(context);
-      //   },
-      //   child: const Icon(Icons.add),
-      //   backgroundColor: Color.fromARGB(255, 104, 104, 104),
-      // ),
     );
   }
-
-//   void _showAddRoomDialog(BuildContext context) {
-//     // Controllers to capture user input
-//     final TextEditingController roomNameController = TextEditingController();
-//     final TextEditingController roomNumberController = TextEditingController();
-//     final TextEditingController roomDetailsController = TextEditingController();
-
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: const Text('Add Room'),
-//           content: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               TextField(
-//                 controller: roomNameController,
-//                 decoration: InputDecoration(labelText: 'Room Name'),
-//               ),
-//               TextField(
-//                 controller: roomNumberController,
-//                 decoration: InputDecoration(labelText: 'Room Number'),
-//                 keyboardType: TextInputType.number,
-//               ),
-//               TextField(
-//                 controller: roomDetailsController,
-//                 decoration: InputDecoration(labelText: 'Room Details'),
-//                 maxLines: 3,
-//               )
-//             ],
-//           ),
-//           actions: [
-//             TextButton(
-//               onPressed: () {
-//                 // Capture the input data
-//                 String roomName = roomNameController.text;
-//                 String roomNumber = roomNumberController.text;
-//                 String roomDetails = roomDetailsController.text;
-
-//                 Navigator.of(context).pop(); // Close the dialog
-//               },
-//               child: const Text('Add'),
-//             ),
-//             TextButton(
-//                 onPressed: () {
-//                   Navigator.of(context).pop();
-//                 },
-//                 child: Text('Close'))
-//           ],
-//         );
-//       },
-//     );
-//   }
 }
 
 class RoomSlot extends StatelessWidget {
@@ -206,41 +120,56 @@ class RoomSlot extends StatelessWidget {
   final String occupancy;
 
   const RoomSlot({
-    Key? key,
+    super.key,
     required this.roomName,
     required this.imagePath,
     required this.occupancy,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Color.fromRGBO(240, 235, 227, 1),
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Container(
-        height: 120, // Larger height for the card
-        padding: const EdgeInsets.all(12),
-        child: Row(
-          children: [
-            // Image with larger size
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
-                imagePath,
-                width: 100,
-                height: 100, // Increased image size
-                fit: BoxFit.cover,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          // Navigate to Timeslots page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Timeslots(
+                roomName: roomName,
+                roomImage: imagePath,
               ),
             ),
-            const SizedBox(width: 16), // Space between image and text
-            Expanded(
-              child: Row(
-                children: [
-                  Column(
+          );
+        },
+        splashColor: Colors.grey.withOpacity(0.3), // Add splash effect
+        borderRadius:
+            BorderRadius.circular(12), // Match with card's border radius
+        child: Card(
+          color: const Color.fromRGBO(240, 235, 227, 1),
+          margin: const EdgeInsets.symmetric(vertical: 4),
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Container(
+            height: 120, // Larger height for the card
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    imagePath,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(width: 16), // Space between image and text
+                Expanded(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -253,7 +182,7 @@ class RoomSlot extends StatelessWidget {
                       ),
                       const SizedBox(height: 8),
                       Text(
-                        '$occupancy',
+                        occupancy,
                         style: const TextStyle(
                           fontSize: 16,
                           color: Colors.grey,
@@ -261,12 +190,196 @@ class RoomSlot extends StatelessWidget {
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
   }
+}
+
+class Timeslots extends StatelessWidget {
+  final String roomName;
+  final String roomImage;
+
+  const Timeslots({
+    Key? key,
+    required this.roomName,
+    required this.roomImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String currentDate = '${now.day}/${now.month}/${now.year}';
+    String currentTime = '${now.hour}:${now.minute.toString().padLeft(2, '0')}';
+
+    List<TimeSlot> timeSlots = [
+      TimeSlot(time: '08:00-10:00', status: 'free'),
+      TimeSlot(time: '10:00-12:00', status: 'reserved'),
+      TimeSlot(time: '13:00-15:00', status: 'disabled'),
+      TimeSlot(time: '15:00-17:00', status: 'pending'),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(roomName),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Date and time
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buildDateTimeCard(Icons.calendar_today, currentDate),
+                  buildDateTimeCard(Icons.access_time, currentTime),
+                ],
+              ),
+              const SizedBox(height: 20),
+
+              // Image of the room
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  roomImage,
+                  width: 150,
+                  height: 150,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              // Time slots
+              Expanded(
+                child: ListView.builder(
+                  itemCount: timeSlots.length,
+                  itemBuilder: (context, index) {
+                    TimeSlot slot = timeSlots[index];
+                    return buildTimeSlot(context, slot);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildDateTimeCard(IconData icon, String text) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(240, 235, 227, 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, size: 18),
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTimeSlot(BuildContext context, TimeSlot slot) {
+    Color backgroundColor;
+    Color textColor = Colors.white;
+    String buttonText;
+
+    switch (slot.status) {
+      case 'free':
+        backgroundColor =
+            const Color.fromRGBO(59, 160, 81, 1); // Green for free
+        buttonText = 'Free';
+        break;
+      case 'pending':
+        backgroundColor =
+            const Color.fromRGBO(255, 191, 97, 1); // Orange for pending
+        buttonText = 'Pending';
+        break;
+      case 'reserved':
+        backgroundColor =
+            const Color.fromRGBO(137, 121, 255, 1); // Purple for reserved
+        buttonText = 'Reserved';
+        break;
+      case 'disabled':
+        backgroundColor = Colors.grey; // Grey for disabled
+        buttonText = 'Disabled';
+        break;
+      default:
+        backgroundColor = const Color.fromRGBO(240, 235, 227, 1);
+        buttonText = '';
+    }
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor, // Button color based on status
+          padding: const EdgeInsets.symmetric(vertical: 14),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () {
+          // Logic to handle slot selection based on status
+          if (slot.status == 'free') {
+            // Action to reserve or book this slot
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Slot ${slot.time} is now pending!')),
+            );
+          } else if (slot.status == 'reserved') {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Slot ${slot.time} is already reserved.')),
+            );
+          } else if (slot.status == 'disabled') {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Slot ${slot.time} is disabled.')),
+            );
+          } else if (slot.status == 'pending') {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                  content: Text('Slot ${slot.time} is pending confirmation.')),
+            );
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Time ${slot.time}',
+                style: TextStyle(color: textColor, fontSize: 16),
+              ),
+              Text(
+                buttonText,
+                style: TextStyle(color: textColor, fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TimeSlot {
+  final String time;
+  String status;
+
+  TimeSlot({
+    required this.time,
+    required this.status,
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:project_mobileapp/Staff/home_staff.dart';
 import 'package:project_mobileapp/User(student)/home.dart';
+import 'package:project_mobileapp/User(student)/timeslots.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Home(),
-    // theme: ThemeData(),
     debugShowCheckedModeBanner: false,
   ));
 }
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: Timeslots(
+//       roomName: 'Meeting Room 1', // Provide a proper room name as a String
+//       roomImage:
+//           'assets/images/meeting.png', // Ensure this is the path to your image
+//     ),
+//     debugShowCheckedModeBanner: false,
+//   ));
+// }
