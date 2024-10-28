75f3dd679889e00286890c84c7cce018229b1787
Srrt <seatsiri.d@gmail.com>
2024-10-29 02:11:23 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
+=======
+import 'package:project_mobileapp/Staff/home_staff.dart';
+import 'package:project_mobileapp/User(student)/Profile.dart';
+>>>>>>> a63c60c5731a59d3c2077f2c00755e3aca1d6e0c
 import 'package:project_mobileapp/User(student)/home.dart';
 import 'package:project_mobileapp/User(student)/timeslots.dart';
 
 void main() {
   runApp(MaterialApp(
+<<<<<<< HEAD
     home: Home(),
+=======
+    home: ProfileApp(),
+    // theme: ThemeData(),
+>>>>>>> a63c60c5731a59d3c2077f2c00755e3aca1d6e0c
     debugShowCheckedModeBanner: false,
   ));
 }
