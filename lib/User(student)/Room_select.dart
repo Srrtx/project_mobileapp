03edf0942de0912133c7ce59d9f5027738449674
ZeenJung <6531501145@lamduan.mfu.ac.th>
2024-10-28 17:23:51 +0700

    Update Room_select.dart

---
+++ lib/User(student)/Room_select.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class select_room extends StatelessWidget {
+  const select_room({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
